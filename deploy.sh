239332e0eb03512409ed511159fda3a927a0f50d
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2019-01-14 19:09:51 +0700

    add site ID in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ zip -r build.zip build
 curl -H "Content-Type: application/zip" \
      -H "Authorization: Bearer $netlify_key" \
      --data-binary "@build.zip" \
-     https://api.netlify.com/api/v1/sites
+     https://api.netlify.com/api/v1/sites/fc7bdcf4-0afc-4bfd-b3a6-cf9a5f020d7e
