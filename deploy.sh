6c5e75e04732fa0147946b4b90276e7a330b4d79
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2019-01-14 18:32:40 +0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
      -H "Authorization: Bearer $netlify_key"
      --data-binary "@build.zip" \
-     http://api.netlify.com/api/v1/sites
+     https://api.netlify.com/api/v1/sites
