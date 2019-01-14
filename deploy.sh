5cc7086f09875f60654e26beb38938a3893b4bf1
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2019-01-14 19:53:16 +0700

    update deploy.sh: add put request

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 set -e
-# zip -r build.zip build
 
-curl -X POST -d {} https://api.netlify.com/build_hooks/5c3c7d665e17125d5191f501
+zip -r build.zip build
+
+curl -X PUT
+     -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $netlify_key" \
+     --data-binary "@build.zip" \
+     https://api.netlify.com/api/v1/sites/fc7bdcf4-0afc-4bfd-b3a6-cf9a5f020d7e
