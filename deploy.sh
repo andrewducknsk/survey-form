0062f70fcf003ee5071afc1872cb7f45c82afe77
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2019-01-14 22:29:05 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -e
 zip -r build.zip build
 
 curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $netlify_key" \
+     -H "Authorization: Bearer $netlify" \
      --data-binary "@build.zip" \
      -X PUT https://api.netlify.com/api/v1/sites/fc7bdcf4-0afc-4bfd-b3a6-cf9a5f020d7e
