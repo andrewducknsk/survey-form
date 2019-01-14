7d2532e6141c828ccd67d15b26eb54f811b1bb3b
andrewducknsk <32197680+andrewducknsk@users.noreply.github.com>
2019-01-14 19:38:05 +0700

    update deploy.sh: change curl request

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 set -e
-zip -r build.zip build
+# zip -r build.zip build
 
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $netlify_key" \
-     --data-binary "@build.zip" \
-     https://api.netlify.com/build_hooks/5c3c7d665e17125d5191f501
+curl -X POST -d {} https://api.netlify.com/build_hooks/5c3c7d665e17125d5191f501
